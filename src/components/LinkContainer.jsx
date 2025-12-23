cce5170c5cc3cba99708c2420d15b5ca2687e32f
hadynj <hadynjiee@gmail.com>
2025-12-23 16:13:25 -0500

    initial commit

---
+++ src/components/LinkContainer.jsx
@@ -0,0 +1,17 @@
+import Table from "./Table"
+import Form from "./Form"
+
+function LinkContainer(){
+    return (
+        <div>
+            <h1>My favorite Links</h1>
+            <p>Add a new link with a name and URL to the table!</p>
+            <Table />
+            <h1>Add New </h1>
+            <Form />
+        
+        </div>
+    )
+}
+
+export default LinkContainer
