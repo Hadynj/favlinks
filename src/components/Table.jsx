3bade3a4b0e74acb8bbbeaea4c99f84a8e8ebb59
hadynj <hadynjiee@gmail.com>
2025-12-24 05:28:55 -0500

    initial commit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -10,10 +10,29 @@ function TableHeader(){
         </table>
     )
 }
-function Tablebody(){
-    /*responsible for rendering the data for our table*/
-    return (
-        <tbody></tbody>
+const TableBody = (props) => {
+    // boilerplate table body functional component
+    // we use Array.map to create table rows from LinkData passed via props
+    const rows = props.linkData.map((row, index) => {
+        return(
+            <tr key={row.id || index}>
+            <td> {row.name}</td>
+            <td>
+                <a href={row.URL}>{row.URL}</a>
+            </td>
+            <td>
+                <button onClick = {() => props.removeLink 
+                    (index)}>Delete
+                </button>
+            </td>
+            </tr>
+        )
+    })
+    
+    return(
+        <tbody>
+            {rows}
+        </tbody>
     )
 }
 
